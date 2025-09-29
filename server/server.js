7389eb542e1b72c406be84f1f49890ec745bcd0c
hadhee17 <hadhee58@gmail.com>
2025-09-29 20:09:46 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ dotenv.config({ path: './config.env' });
 
 const port = process.env.PORT || 8000;
 
+app.use('/', (req, res) => {
+  res.send('API is running....');
+});
+
 (async function start() {
   try {
     await connectDB(); // wait for DB connection
